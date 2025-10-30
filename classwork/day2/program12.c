b33fba4a26929357bf5db9550c2471516a7061eb
Aatharvajoshii <joshiatharva53@gmail.com>
2025-10-30 11:01:05 +0530

    Add files via upload

---
+++ classwork/day2/program12.c
@@ -0,0 +1,38 @@
+/*
+    START
+        Accept number and store as no
+        divide no by
+        if the remainder is zero
+            then display as even
+        otherwise
+            display as odd
+    STOP
+*/
+
+#include<stdio.h>
+
+void CheckEvenOdd(int iNo)
+{
+    int iRem = 0;
+    iRem = iNo % 2;
+
+    if(iRem == 0)
+    {
+        printf("It is even number");
+    }
+    else
+    {
+        printf("It is odd number");
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    CheckEvenOdd(iValue);
+
+    return 0;
+}
---
+++ classwork/day2/program16.c
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Required header files
+//
+//////////////////////////////////////////////////////////////////////////// 
+
+#include<stdio.h>                           // for input output
+#include<stdbool.h>                         // for bool datatye
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  function name :  CheckEvenOdd
+//
+//
+//
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////
+
+bool CheckEvenOdd(int iNo)
+{
+    int iRem = 0;
+    iRem = iNo % 2;
+
+    if(iRem == 0)
+    {return true;}
+    else
+    {return false;}
+}
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function of the application
+//
+////////////////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {printf("%d is even number",iValue);}
+    else
+    {printf("%d is odd number",iValue);}
+    
+
+    return 0;
+}
