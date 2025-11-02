5fd7d8e15cf3c5808a67343e1cb9fe6b77417118
Aatharvajoshii <joshiatharva53@gmail.com>
2025-11-02 21:57:45 +0530

    Add files via upload

---
+++ Assignments/Assignment 3/problem3.c
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////
+//  Function Name : DisplayFactor
+//  Description :   Accept number from user and display that number of * on screen
+//  Input :         int
+//  Output :        void
+//  Author :        Atharva joshi
+//  Date :          09/10/2025
+// 
+/////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void DisplayFactor(int iNo)
+{
+    int i = 0;
+
+    if (iNo <= 0)
+    {
+        iNo = -iNo;   // convert negative to positive
+    }
+
+    for (i = 1; i <= iNo / 2; i++)   // exclude the number itself
+    {
+        if ((iNo % i == 0) && (i % 2 == 0))
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    DisplayFactor(iValue);
+
+    return 0;
+}
