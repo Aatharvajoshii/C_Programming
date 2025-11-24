0d63d0692f13362fdd5b68e0edec7e98ce9f27af
Aatharvajoshii <joshiatharva53@gmail.com>
2025-11-24 14:54:35 +0530

    Add files via upload

---
+++ Assignments/Assignment26/problem2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void Pattern(int iNo) {
+    for(int i = iNo; i >= 1; i--) {
+        printf("%d\t#\t", i);
+    }
+    printf("\n");
+}
+
+int main() {
+    int iValue = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &iValue);
+    Pattern(iValue);
+    return 0;
+}
