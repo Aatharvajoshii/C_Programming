d91a00059462a011f65235552f59e84555dc3d2b
Aatharvajoshii <joshiatharva53@gmail.com>
2025-12-04 14:37:04 +0530

    Add files via upload

---
+++ classwork/day12/program209.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char Arr[] = "hello";
+
+    printf("%s\n",Arr);
+
+    return 0;
+}
---
+++ classwork/day12/program210.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    char Arr[] = {'h','e','l','l','o'};
+
+    printf("%s\n",Arr);
+
+    return 0;
+}
